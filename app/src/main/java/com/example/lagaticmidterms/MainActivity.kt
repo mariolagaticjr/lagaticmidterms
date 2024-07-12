44ebbe208b0352858307884c36d20c65e1269862
dalepantano <dalepantano54@gmail.com>
2024-07-12 21:32:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lagaticmidterms/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.lagaticmidterms
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Apply window insets
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Find the button and set the OnClickListener
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            // Create an Intent to start MainActivity2
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
